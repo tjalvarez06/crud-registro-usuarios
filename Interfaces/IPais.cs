ef90a2c862f6ed8504e36b258f796d0be287fb77
tjalvarez06 <tomasalvarez06@hotmail.com>
2025-04-04 21:17:49 -0400

    Agregadas interfaces IRepository, IUsuarios y IPais

---
+++ Interfaces/IPais.cs
@@ -0,0 +1,13 @@
+﻿using RegistroUsuarios.Models;
+
+namespace RegistroUsuarios.Interfaces
+{
+    public interface IPais
+    {
+        string CreatePais(Pais model);
+        string UpdatePais(Pais model);
+        string DeletePais(int id);
+        List<Pais> GetPaises();
+        Pais GetPaisById(int id);
+    }
+}
---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RegistroUsuarios.Interfaces
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        void Set(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        IActionResult Login(string correo, string clave);
+
+    }
+}
